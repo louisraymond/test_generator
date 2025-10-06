53fca55377a760cccaad0956dab4027fc2f2600c
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2025-10-07 00:27:18 +0100

    feat: add exam template views and section breaks
    - Create template management views (index, show, new, edit)
    - Add comprehensive template form with nested sections
    - Implement dynamic section/source/question rule builders
    - Add Stimulus controller for template form interactions
    - Display section breaks in exam show view
    - Display section breaks in marking scheme
    - Add section-aware rendering with metadata
    - Style section headers with gradient backgrounds
    - Add navigation link for Exam Templates
    - Support viewing template usage statistics

---
+++ app/javascript/controllers/template_form_controller.js
@@ -0,0 +1,166 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [
+    "sectionsContainer",
+    "sectionTemplate",
+    "sectionCard",
+    "sourceRuleField",
+    "questionRuleField"
+  ]
+
+  connect() {
+    this.sectionIndex = this.sectionCardTargets.length
+  }
+
+  addSection(event) {
+    event.preventDefault()
+    
+    const content = this.sectionTemplateTarget.innerHTML
+    const newSection = content.replace(/NEW_SECTION_RECORD/g, new Date().getTime())
+    
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = newSection
+    
+    // Update position field
+    const positionInput = wrapper.querySelector('input[name*="[position]"]')
+    if (positionInput) {
+      positionInput.value = this.sectionIndex
+    }
+    
+    this.sectionsContainerTarget.insertAdjacentHTML('beforeend', wrapper.innerHTML)
+    this.sectionIndex++
+  }
+
+  removeSection(event) {
+    event.preventDefault()
+    const card = event.target.closest('.section-card')
+    const destroyField = card.querySelector('input[name*="[_destroy]"]')
+    
+    if (destroyField) {
+      destroyField.value = '1'
+      card.style.display = 'none'
+    } else {
+      card.remove()
+    }
+  }
+
+  addSourceRule(event) {
+    event.preventDefault()
+    const button = event.target.closest('button')
+    const sectionIndex = button.dataset.sectionIndex
+    const container = button.previousElementSibling
+    
+    const newId = new Date().getTime()
+    const template = `
+      <div class="rule-field" data-template-form-target="sourceRuleField">
+        <input type="hidden" name="exam_template[exam_sections_attributes][${sectionIndex}][section_source_rules_attributes][${newId}][_destroy]" value="false">
+        
+        <div class="rule-field__row">
+          <div class="rule-field__select-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_source_type">Type</label>
+            <select class="rule-field__select" name="exam_template[exam_sections_attributes][${sectionIndex}][section_source_rules_attributes][${newId}][source_type]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_source_type">
+              <option value="">Select type</option>
+              <option value="Topic">Topic</option>
+              <option value="TopicModule">Module</option>
+              <option value="LearningObjective">Learning Objective</option>
+            </select>
+          </div>
+
+          <div class="rule-field__select-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_source_id">Source</label>
+            <select class="rule-field__select" name="exam_template[exam_sections_attributes][${sectionIndex}][section_source_rules_attributes][${newId}][source_id]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_source_id">
+              <option value="">Select source</option>
+            </select>
+          </div>
+
+          <div class="rule-field__input-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_weight">Weight</label>
+            <input class="rule-field__input" type="number" min="1" value="1" name="exam_template[exam_sections_attributes][${sectionIndex}][section_source_rules_attributes][${newId}][weight]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_weight">
+          </div>
+
+          <div class="rule-field__input-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_question_count_override">Exact Count</label>
+            <input class="rule-field__input" type="number" min="0" placeholder="Optional" name="exam_template[exam_sections_attributes][${sectionIndex}][section_source_rules_attributes][${newId}][question_count_override]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_source_rules_attributes_${newId}_question_count_override">
+          </div>
+
+          <button type="button" class="btn btn--small btn--danger rule-field__remove" data-action="click->template-form#removeSourceRule">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+          </button>
+        </div>
+      </div>
+    `
+    
+    container.insertAdjacentHTML('beforeend', template)
+  }
+
+  removeSourceRule(event) {
+    event.preventDefault()
+    const field = event.target.closest('.rule-field')
+    const destroyField = field.querySelector('input[name*="[_destroy]"]')
+    
+    if (destroyField && destroyField.value !== 'false') {
+      destroyField.value = '1'
+      field.style.display = 'none'
+    } else {
+      field.remove()
+    }
+  }
+
+  addQuestionRule(event) {
+    event.preventDefault()
+    const button = event.target.closest('button')
+    const sectionIndex = button.dataset.sectionIndex
+    const container = button.previousElementSibling
+    
+    const newId = new Date().getTime()
+    const template = `
+      <div class="rule-field" data-template-form-target="questionRuleField">
+        <input type="hidden" name="exam_template[exam_sections_attributes][${sectionIndex}][section_question_rules_attributes][${newId}][_destroy]" value="false">
+        
+        <div class="rule-field__row">
+          <div class="rule-field__select-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_rule_type">Rule Type</label>
+            <select class="rule-field__select" name="exam_template[exam_sections_attributes][${sectionIndex}][section_question_rules_attributes][${newId}][rule_type]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_rule_type">
+              <option value="">Select type</option>
+              <option value="force_include">Force Include</option>
+              <option value="exclude">Exclude</option>
+            </select>
+          </div>
+
+          <div class="rule-field__select-group" style="grid-column: span 2;">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_question_id">Question</label>
+            <select class="rule-field__select" name="exam_template[exam_sections_attributes][${sectionIndex}][section_question_rules_attributes][${newId}][question_id]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_question_id">
+              <option value="">Select question</option>
+            </select>
+          </div>
+
+          <div class="rule-field__input-group">
+            <label class="rule-field__label" for="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_repeat_count">Repeats</label>
+            <input class="rule-field__input" type="number" min="1" value="1" name="exam_template[exam_sections_attributes][${sectionIndex}][section_question_rules_attributes][${newId}][repeat_count]" id="exam_template_exam_sections_attributes_${sectionIndex}_section_question_rules_attributes_${newId}_repeat_count">
+          </div>
+
+          <button type="button" class="btn btn--small btn--danger rule-field__remove" data-action="click->template-form#removeQuestionRule">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+          </button>
+        </div>
+      </div>
+    `
+    
+    container.insertAdjacentHTML('beforeend', template)
+  }
+
+  removeQuestionRule(event) {
+    event.preventDefault()
+    const field = event.target.closest('.rule-field')
+    const destroyField = field.querySelector('input[name*="[_destroy]"]')
+    
+    if (destroyField && destroyField.value !== 'false') {
+      destroyField.value = '1'
+      field.style.display = 'none'
+    } else {
+      field.remove()
+    }
+  }
+}
+
